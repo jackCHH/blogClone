a36119feb495e23434964e877a7c0c115f2381ef
Jack Hou <jack.hou@live.com>
2015-03-09 15:30:21 +1300

    Implement Edit and Delete Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,29 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render "edit"
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		# make sure it's not post_path (singular) since we want to see ALL THE POSTS after we delete a post. If we only 
+		# use post_path, it will try to redirect to the already deleted post, thus rendering an error
+		redirect_to posts_path
+	end
+
 	private
 
 	def post_params
