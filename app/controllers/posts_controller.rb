31b71a6a55884cf14238847944b1accf5dde8476
Jack Hou <jack.hou@live.com>
2015-03-09 14:58:13 +1300

    Added Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,29 @@
 class PostsController < ApplicationController
 	def index
-
+		@post = Post.all.order("created_at DESC")
 	end
 
 	# let's add the ability to create new posts
 	def new
-		
+
+	end
+
+
+	def create
+		# post_param is a safe guard. forgot what it's called.
+		@post = Post.new(post_params)
+		@post.save
+		#after we save, redirect to the post we just created. that's why it's redirect to @post. 
+		redirect_to @post
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body)
 	end
 end
