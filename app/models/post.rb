c128f672c8ac63d7ddcbeac018a0bc05805f33e4
Jack Hou <jack.hou@live.com>
2015-03-09 15:51:01 +1300

    Add Comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
 	# let's add some validation to prevent empty posts
 	validates :title, presence: true
 	validates :body, presence: true
+
 end
